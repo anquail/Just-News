a1a49de8d344e34f90bfd7547ae38eb4a19e3432
Angelo <quailangelo@gmail.com>
2021-03-02 19:22:42 -0500

    updated scripts and fixed build issue in production

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use("/build", express.static(path.join(__dirname, "../build/build.js")));
+app.use("/build", express.static(path.join(__dirname, "../build/")));
 
 app.get("/", (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, "../index.html"));
