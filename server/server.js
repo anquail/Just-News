8d4e7c57c4821b530d73808218777dd5e1b42e81
Angelo <quailangelo@gmail.com>
2021-03-02 19:25:35 -0500

    added config for production

--- server/server.js
+++ server/server.js
@@ -3,10 +3,11 @@ const path = require("path");
 
 const app = express();
 
-app.use("/build", express.static(path.join(__dirname, "../build/")));
+if (process.env.NODE_ENV === "production") {
+  app.use("/build", express.static(path.join(__dirname, "../build/")));
 
-app.get("/", (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, "../index.html"));
-});
-
+  app.get("/", (req, res) =>
+    res.status(200).sendFile(path.join(__dirname, "../index.html"))
+  );
+}
 app.listen(3000);
